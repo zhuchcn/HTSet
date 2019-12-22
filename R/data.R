07ae11c6a9a5fc2a7359aa45ceedb5c22b69e118
Chenghao Zhu <zhuchcn@gmail.com>
2019-12-22 11:08:25 -0800

    add data lipidome and exrna

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' @title lipidomics dataset from human isolated HDL
+#'
+#' @description A \code{\link{HTSet-class}} dataset of lipidomics analysis
+#' from isolated HDL from 10 human subjects with 2x2 cross-over study design.
+#' The data was obtained from the article published on \emph{Metabolomics} in
+#' 2019 (doi.org/10.1007/s11306-019-1579-1)
+#'
+#' @docType data
+#' @keywords datasets
+#' @name lipidome
+#' @usage data(lipidome)
+#' @format A \code{\link{HTSet-class}} object
+#'
+#' @references
+#' Zhu, C. et al., The HDL lipidome is widely remodeled by fast food versus
+#' Mediterranean diet in 4 days. \emph{Metabolomics}, 2019 Aug 17;15(8):114.
+#' doi: 10.1007/s11306-019-1579-1.
+"lipidome"
+
+#' @title exRNA from human isolated HDL
+#' @description extra-cellular small RNA-seq gene counts from isolated HDL
+#' from 3 human patients with systemic lupus erythematosus and 3 healthy
+#' control.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name exrna
+#' @usage data(exrna)
+#' @format A \code{\link{HTSet-class}} object
+#'
+#' @references https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE121865
+"exrna"
